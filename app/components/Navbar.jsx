a35ff127da87c77d8e7af86775a213c2b6c34b03
Cara Weber <caraweber@hotmail.com>
2016-11-28 10:57:46 -0500

    few changes

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,10 +6,12 @@ export default class Navbar extends React.Component {
 
 render () {
 return (
-        <nav id="myNavBar" className="navbar  navbar-static-top">
-  <div className="container-fluid">
+    
+  <nav className="myNavBar navbar  navbar-static-top">
+  <div className="myNavBar container-fluid">
+  
     {/* <!-- Brand and toggle get grouped for better mobile display --> */}
-    <div className="navbar-header">
+    <div className="myNavBar navbar-header">
       <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span className="sr-only">Toggle navigation</span>
         <span className="icon-bar"></span>
@@ -20,8 +22,8 @@ return (
     </div>
 
     {/*<!-- Collect the nav links, forms, and other content for toggling -->*/}
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul className="nav navbar-nav">
+    <div className="myNavBar collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="myNavBar nav navbar-nav">
         <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
         <li><a href="#">Link</a></li>
 
@@ -38,9 +40,9 @@ return (
           </ul>
         </li>
       </ul>
-    </div>{/*<!-- /.navbar-collapse -->*/}
+    </div>{/*<!-- /.navbar-collapse -->*/}  
   </div>{/*<!-- /.container-fluid -->*/}
-</nav>
+</nav>  
     )
 }
 }
